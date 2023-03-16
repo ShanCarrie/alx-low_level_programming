65237b4ea373933e7816e935698905baeb3aabf8
Shanelle Omondi <shanelleomondi@gmail.com>
2023-03-16 12:11:45 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <studio.h>
+/**
+ * main -it runs the statement inside it
+ *
+ * Return: 0 to exit the function
+ */
+int main(void)
+{
+puts("/"Programming is like building a multilingual puzzle");
+return (0);
+}
