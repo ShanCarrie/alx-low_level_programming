bbf4a7af95d458bd17f173731590b8b5ebb73650
Shanelle Omondi <shanelleomondi@gmail.com>
2023-03-16 12:36:39 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ * main - runs the statements in it
+ * Return: 0 to exit the function
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
