186736cf8c1c2f72640f1c2d773c198643150c02
Shanelle Omondi <shanelleomondi@gmail.com>
2023-03-19 06:35:23 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,11 +7,17 @@
 int main(void)
 {
 	char ch = 'a';
+	char CH = 'A';
 
 	while (ch <= 'z')
 	{
 		putchar(ch);
 		ch++;
+	}
+	while (CH <= 'Z')
+	{
+		putchar(CH);
+		CH++;
 	}
 	putchar('\n');
 	return (0);
