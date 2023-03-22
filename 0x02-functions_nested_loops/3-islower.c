d92ea8553ac79150bd1028ea91ab8639e213d1ce
Shanelle Omondi <shanelleomondi@gmail.com>
2023-03-22 06:08:00 -0700

    nested

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -6,3 +6,14 @@
  * Return: 1 if char is lowecase, otherwise 0;
  */
 int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _isalpha - shpes 1 if the input is a letter
+ * Otherwise shows 0
+ * @c: The character in ASCii code
+ * Return 1: for rletters. 0 for the rest
+ */
+int _isalpha(int c)
+{
+	if (( c >= 97 && c <= 122) || (c >=65 && x <= 90))
+	{ return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n')
+}
