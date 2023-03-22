157b359f1f346a6c02749866e8c7c32c3956191f
Shanelle Omondi <shanelleomondi@gmail.com>
2023-03-22 09:34:03 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,14 +2,15 @@
 
 /**
  * print_alphabet - print all alphabet in lowercase
+ * Return: always 0
  */
 void print_alphabet(void)
 {
 	int i;
 
-	for (i = 97; i  <= 122; i++)
+	for (i = 97; i <= 122; i++)
 	{
-			_putchar(i);
+		_putchar(i);
 	}
 	_putchar('\n');
 }
